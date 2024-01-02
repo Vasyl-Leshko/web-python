a9caffc6f975a5399d22b920cdc4fb7024edf5f9
Vasyl-Leshko <152077542+Vasyl-Leshko@users.noreply.github.com>
2024-01-02 17:51:56 +0200

    Add files via upload

---
+++ lab3/data.py
@@ -0,0 +1,18 @@
+skills = [
+    {
+        'Name': 'Python',
+        'Level': '20',
+    },
+    {
+        'Name': 'JavaScript',
+        'Level': '10',
+    },
+    {
+        'Name': 'C++',
+        'Level': '30',
+    },
+    {
+        'Name': 'HTML/CSS',
+        'Level': '50',
+    },
+]
