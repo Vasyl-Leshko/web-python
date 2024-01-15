5c18c54268b29a5ec977b3457ba0a83230abdc8d
Vasyl-Leshko <152077542+Vasyl-Leshko@users.noreply.github.com>
2024-01-15 02:35:18 +0200

    Add files via upload

---
+++ lab7/app/__init__.py
@@ -0,0 +1,29 @@
+import json
+from flask import Flask
+import platform
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.secret_key = 'klchJ89Bch'
+
+bcrypt = Bcrypt(app)
+
+os_info = platform.system()
+current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+with open('users.json') as f:
+    users = json.load(f)
+
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+from app import models
+with app.app_context():
+    db.create_all()
+
+from app import views
